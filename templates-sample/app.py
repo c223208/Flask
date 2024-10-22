3f588d81b467a60b3364cbe471faa63adc26e733
c223208 <c223208@ed.nagoya-cu.ac.jp>
2024-10-22 09:56:05 +0900

    Initial commit

--- templates-sample/app.py
+++ templates-sample/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 # インスタンス生成
 app = Flask(__name__)
 
+
 # ルーティング
 # TOPページ
 @app.route('/')
@@ -55,6 +56,29 @@ class Hero:
 def show_jinja_class():
     hana = Hero('花咲かじいさん', 99)
     return render_template('jinja/show4.html', user = hana)
+
+#▼▼▼▼▼ここから【制御文】▼▼▼▼▼
+# 「商品」クラス
+class item:
+    # コンストラクタ
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+    # 表示用関数
+    def __str__(self):
+        return f'商品ID：{self.id} 商品名：{self.name}'
+
+# 繰り返し
+@app.route("/for_list")
+def show_for_list():
+    item_list = [item(1,"ダンゴ"), item(2,"にくまん"), item(3,"ドラ焼き")]
+    return render_template('for_list.html', items = item_list)
+
+# 条件分岐
+@app.route('/if_detail/<int:id>')
+def show_if_detail(id):
+    item_list = [item(1,"ダンゴ"), item(2,"にくまん"), item(3,"ドラ焼き")]
+    return render_template('if_detail.html', show_id=id, items = item_list)
 
 # 実行
 if __name__ == '__main__':
