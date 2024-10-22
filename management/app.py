94b927fbeb74da59cb2d25f86eb217001335fb0c
c223208 <c223208@ed.nagoya-cu.ac.jp>
2024-10-22 14:25:50 +0900

    Initial commit

--- management/app.py
+++ management/app.py
@@ -2,3 +2,25 @@ from flask import Flask, render_template
 
 # インスタンス生成
 app = Flask(__name__)
+
+# ルーティング
+# TOPページ
+@app.route('/')
+def index():
+    return render_template('jinja/top.html')
+
+# 一覧
+class member:
+    # コンストラクタ
+    def __init__(self, id, name):
+        self.id = id
+
+@app.route('/list')
+def member_list():
+    return render_template
+
+# 詳細
+
+# 実行
+if __name__ == '__main__':
+    app.run()
